597a9d1567ae252cab136454fa2cda145b110f25
Niall Quinn <niall@niallquinn.me>
2017-09-20 10:26:35 +0100

    Typo

--- src/ios/Popdeem.m
+++ src/ios/Popdeem.m
@@ -3,13 +3,10 @@
 @implementation Popdeem
 
 - (void) enableSocialLogin:(CDVInvokedUrlCommand*)command {
-  NSInteger* numberOfPrompts = [[[command arguments] objectAtIndex:0] integerValue];
-
-  // [PopdeemSDK enableSocialLoginWithNumberOfPrompts:numberOfPrompts];
 
   CDVPluginResult* result = [CDVPluginResult
                             resultWithStatus:CDVCommandStatus_OK
-                            messageAsString:"Presented Social Login"];
+                            messageAsString:@"Presented Social Login"];
 
   [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
