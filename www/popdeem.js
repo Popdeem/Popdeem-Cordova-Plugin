39507c65093b88b950fd4ecb5907548bb826259f
Niall Quinn <niall@niallquinn.me>
2017-09-26 11:07:49 +0100

    Remove 1

--- www/popdeem.js
+++ www/popdeem.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-  pushPopdeemHome: function (successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Popdeem", "pushPopdeemHome", []);
-  };
+  // pushPopdeemHome: function (successCallback, errorCallback) {
+  //   cordova.exec(successCallback, errorCallback, "Popdeem", "pushPopdeemHome", []);
+  // };
 
   enableSocialLogin: function (numberOfPrompts, successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "Popdeem", "enableSocialLogin", [numberOfPrompts]);
