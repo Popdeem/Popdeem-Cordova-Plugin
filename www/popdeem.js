364c87b2feb0d4cdf54a29983be7ac47e69cb6c8
Niall Quinn <niall@niallquinn.me>
2017-09-26 10:29:30 +0100

    Update syntax

--- www/popdeem.js
+++ www/popdeem.js
@@ -1,8 +1,8 @@
 module.exports = {
     enableSocialLogin: function (numberOfPrompts, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Popdeem", "enableSocialLogin", [numberOfPrompts]);
-    },
+    };
     pushPopdeemHome: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Popdeem", "pushPopdeemHome", []);
-    }
+    };
 };
