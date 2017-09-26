5e7b016627cafac1f2b1c8000e6332f51d1d88c3
Niall Quinn <niall@niallquinn.me>
2017-09-26 12:08:23 +0100

    Device Ready Bug

--- www/popdeem.js
+++ www/popdeem.js
@@ -2,6 +2,6 @@ module.exports = {
 
   enableSocialLogin: function (numberOfPrompts, successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "Popdeem", "enableSocialLogin", [numberOfPrompts]);
-  };
+  }
 
 };
