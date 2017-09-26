7eabc7fdb028e2e49d7eaf32e8eba2e5c6a85004
Niall Quinn <niall@niallquinn.me>
2017-09-26 13:03:55 +0100

    Update PopdeemSDK Version

--- www/popdeem.js
+++ www/popdeem.js
@@ -2,12 +2,7 @@ module.exports = {
 
   enableSocialLogin: function (numberOfPrompts, successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "Popdeem", "enableSocialLogin", [numberOfPrompts]);
-  }
-
-};
-
-module.exports = {
-
+  },
   pushPopdeemHome: function (successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "Popdeem", "pushPopdeemHome", []);
   }
