6b6c54ecf62748b3d0c7b20eb59b10cefddc189f
Niall Quinn <niall@niallquinn.me>
2017-09-28 13:00:21 +0100

    Add Popdeem Toast

--- src/com/popdeem/cordova/plugin/Popdeem.java
+++ src/com/popdeem/cordova/plugin/Popdeem.java
@@ -7,7 +7,11 @@ public class Popdeem extends CordovaPlugin {
 
   private void enableSocialLogin(String message, CallbackContext callbackContext) {
     if (message != null && message.length() > 0) {
-        //TODO Push Popdeem Social Login
+      CharSequence text = "Hello toast!";
+      int duration = Toast.LENGTH_SHORT;
+
+      Toast toast = Toast.makeText(callbackContext, text, duration);
+      toast.show();
     } else {
         callbackContext.error("Expected one non-empty string argument.");
     }
