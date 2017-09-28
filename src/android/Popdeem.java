7b85440caee47dedd6c9c024d64c3be3ff899120
Niall Quinn <niall@niallquinn.me>
2017-09-28 16:07:16 +0100

    Add coming soon messafe for Android

--- src/android/Popdeem.java
+++ src/android/Popdeem.java
@@ -13,25 +13,27 @@ public class Popdeem extends CordovaPlugin {
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-      if ("enableSocialLogin".equals(action)) {
-          this.enableSocialLogin(args.getInt(0), callbackContext);
-          callbackContext.success();
-          return true;
-      }
+    this.showComingSoon(callbackContext);
+      // if ("enableSocialLogin".equals(action)) {
+      //     this.enableSocialLogin(args.getInt(0), callbackContext);
+      //     callbackContext.success();
+      //     return true;
+      // }
       return false;  // Returning false results in a "MethodNotFound" error.
   }
 
+  private void showComingSoon(CallbackContext callbackContext) {
+    String message = "Android Coming Soon";
+    int duration = Toast.LENGTH_SHORT;
+    Context context=this.cordova.getActivity().getApplicationContext();
+    Toast toast = Toast.makeText(context, text, duration);
+    toast.show();
+  }
+
   private void enableSocialLogin(int numberOfPrompts, CallbackContext callbackContext) {
-      String message = "Popdeem WORKS";
-    if (message != null && message.length() > 0) {
-      CharSequence text = "Hello toast!";
-      int duration = Toast.LENGTH_SHORT;
-        Context context=this.cordova.getActivity().getApplicationContext();
-        Toast toast = Toast.makeText(context, text, duration);
-      toast.show();
-    } else {
-        callbackContext.error("Expected one non-empty string argument.");
-    }
+  }
+
+  private void pushPopdeemHome(CallbackContext callbackContext) {
   }
 
 }
